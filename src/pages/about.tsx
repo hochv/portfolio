be370409e5ad2816b8178381f2ceb9830b10102d
Học Hoàng Văn <hochv@toprate.io>
2023-05-31 15:05:49 +0700

    feat: set up scss and variables

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import "@styles/global.scss";
+interface IProps {}
+
+/**
+ * @author
+ * @function @About
+ **/
+
+const About: FC<IProps> = (props) => {
+  return <div className="div">About</div>;
+};
+
+export default About;
