c0b2b97a136b0d61ba10a8629721827f635565da
hochv <hochv2001@gmail.com>
2023-06-06 22:21:52 +0700

    feat: change project data

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React, { FC } from "react";
 import telekom from "@img/telekom.jpg";
+import edufit from "@img/edufit.png";
+import HLP from "@img/HLP.jpg";
 import { Poppins } from "next/font/google";
 
 interface IProps {}
@@ -14,34 +16,62 @@ interface IProps {}
 const font = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const Projects: FC<IProps> = (props) => {
+  const PROJECTS_DATA = [
+    {
+      name: "Services Portal (sp.de)",
+      desc: "An E-commerce web app for publishing and selling services, projects, news,... using NextJs. Feels nice to work with foreign developer (Germans) at T-mobile, I also work with them workflow, testing and many around.",
+      img: telekom,
+    },
+    {
+      name: "Services Portal management",
+      desc: "A management web app for manage services, files, roles, news, projects, CMS layout, etc... for Services Portal pages using Angular. Feels nice to work with foreign developer (Germans) at T-mobile, I also work with them workflow, testing and many around.",
+      img: telekom,
+    },
+    {
+      name: "HLP NFTs - market, freelancer",
+      desc: "An E-commerce web app for managing, generating, publishing and advertising NFTs of DAO Peace, using new Nextjs intergrate with Web3 and Blockchain. This is making for Japanese customer then I can understand and feel more easy when facing with their workflow.",
+      img: HLP,
+    },
+    {
+      name: "Edufit - Education management",
+      desc: "A web app for manage and support for students, teacher, parents and all around education, using new Reactjs, Most featured fuction that have are attendance, timeline, schedule, form, and news, etc...",
+      img: edufit,
+    },
+  ];
   return (
     <section className={font.className}>
       <section className="projects">
-        <ProjectBox left />
-        <ProjectBox />
-        <ProjectBox left />
-        <ProjectBox />
+        {PROJECTS_DATA.map((e, idx) => {
+          return (
+            <ProjectBox key={"project__idx" + idx} left={!(idx % 2)} {...e} />
+          );
+        })}
       </section>
     </section>
   );
 };
 
-const ProjectBox = ({ left }: { left?: boolean }) =>
+const ProjectBox = ({
+  left,
+  name,
+  desc,
+  img,
+}: {
+  left?: boolean;
+  name: string;
+  desc: string;
+  img: StaticImageData;
+}) =>
   left ? (
     <div className=" project__box projects__box--left">
       <div className="left">
         <div className="theme-color label__project">Featured Project</div>
-        <div className="project__name">Example Project</div>
-        <div className="project__desc">
-          A web app for visualizing personalized Spotify data. View your top
-          artists, top tracks, recently played tracks, and detailed audio
-          information about each track. Create and save new playlists of
-          recommended tracks based on your existing playlists and more.
-        </div>
+        <div className="project__name">{name}</div>
+        <div className="project__desc">{desc}</div>
       </div>
       <div className="right">
         <Image
-          src={telekom}
+          src={img}
           alt="my portfolio project image"
           width={500}
           className="image"
@@ -52,17 +82,12 @@ const ProjectBox = ({ left }: { left?: boolean }) =>
     <div className="  project__box projects__box--right">
       <div className="right">
         <div className="theme-color label__project">Featured Project</div>
-        <div className="project__name">Example Project</div>
-        <div className="project__desc">
-          A web app for visualizing personalized Spotify data. View your top
-          artists, top tracks, recently played tracks, and detailed audio
-          information about each track. Create and save new playlists of
-          recommended tracks based on your existing playlists and more.
-        </div>
+        <div className="project__name">{name}</div>
+        <div className="project__desc">{desc}</div>
       </div>
       <div className="left">
         <Image
-          src={telekom}
+          src={img}
           alt="my portfolio project image"
           width={500}
           className="image"
